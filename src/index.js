106c7de499bbf7e30ae683cdef9b3cc276f09ed3
Pankrusheva Anastasiia <a.pankrusheva@gmail.com>
2020-11-07 08:41:18 +0200

    not so bad

--- src/index.js
+++ src/index.js
@@ -1,6 +1,51 @@
 import './styles.css';
-/*
-new CountdownTimer({
+
+class CountdownTimer {
+    constructor({selector,targetDate}) {
+        this.selector = selector;
+        this.targetDate = targetDate;
+
+        this.refs = {
+            days: document.querySelector('span[data-value="days"]'),
+            hours: document.querySelector('span[data-value="hours"]'),
+            mins: document.querySelector('span[data-value="mins"]'),
+            secs: document.querySelector('span[data-value="secs"]'),
+            timerFace: document.querySelector(`${selector}`),
+        };
+    }
+
+    renderCountdownTimer() {
+        setInterval(() => {
+            this.currentTime = Date.now();
+            const deltaTime = this.targetDate - this.currentTime;
+            this.updateCountdownface(getTimeComponents(deltaTime));
+        }, 1000);
+    }
+
+    updateCountdownface({ days, hours, mins, secs }) {
+    this.refs.days.textContent = days;
+    this.refs.hours.textContent = hours;
+    this.refs.mins.textContent = mins;
+    this.refs.secs.textContent = secs;
+}
+};
+
+const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
-});*/
+  targetDate: new Date('Nov 8, 2020'),
+});
+
+timer.renderCountdownTimer();
+
+function pad(value) {
+    return String(value).padStart(2, '0');
+}
+
+function getTimeComponents(time) {
+const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+}
